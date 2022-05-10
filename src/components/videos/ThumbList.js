7d27ad5f0c4fea7f61e6cb628a782cae973f1360
jeongboram <pink0913@nate.com>
2022-05-10 11:29:41 +0900

    하단 스크롤 도달시 더보기 기능 추가

--- src/components/videos/ThumbList.js
+++ src/components/videos/ThumbList.js
@@ -8,16 +8,16 @@ import Loader from 'components/Loader';
 const ThumbList = () => {
 	const [target, setTarget] = useState(null);
 	const [videos, setVideos] = useState([]);
-	const [resultNum, setResultNum] = useState(25);
 	const [isLoaded, setIsLoaded] = useState(false);
 	
+	let resultNum = 25
 
 	const getVideos = async () => {
 		setIsLoaded(true);
 		await axios
 			.get(`${baseUrl}/videos?part=snippet&chart=mostPopular&maxResults=${resultNum}&key=${API_KEY}`)
 			.then((res) => {
-				setVideos((videos) => videos.concat(res.data.items))
+				setVideos(res.data.items)
 			})
 			.catch((e) => {
 				console.log('Error:', e);
@@ -25,35 +25,21 @@ const ThumbList = () => {
 		setIsLoaded(false);
 	};
 
-
-	// const onViewMore = () => {
-	// 	console.log('=== 더보기 버튼 클릭 ===');
-    //     console.log('현재 리스트 갯수', resultNum)
-    //     setResultNum(resultNum + 5);
-    //     if ( resultNum >= 50 ) {
-    //         document.querySelector('.btn-moreview').setAttribute('disabled', 'disabled')
-    //     }
-	// 	console.log('itemRef', itemRef)
-	// };
-
 	const onIntersect = async ([entry], observer) => {
 		if (entry.isIntersecting && !isLoaded) {
 			observer.unobserve(entry.target)
-			await getVideos()
+			resultNum += 5
+			await getVideos();
+			if ( resultNum >= 50 ) return   // Max 50개
 			observer.observe(entry.target);
 		}
 	};
 
 	useEffect(() => {
-		console.log('useEffect getVideos() Update')
-	
-	}, []);
-	
-	useEffect(() => {
 		let observer;
 		if (target) {
 			observer = new IntersectionObserver(onIntersect, {
-				threshold: 0.5,
+				threshold: 0.5,     // 해당 타겟이 50% 보여졌을때 실행.
 			});
 			observer.observe(target);
 		}
